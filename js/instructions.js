f740b6dd672d9463a0e50dd7b197b3e691747b9a
FabioBnt <fabiobentaila@gmail.com>
2022-12-02 06:27:02 +0100

    Corrections orthographiques

--- js/instructions.js
+++ js/instructions.js
@@ -4,42 +4,20 @@ var nb_car = 0;
 var chaine = "";
 var id;
 
-function changeMessage() {
-  nb_msg++;
-  if (nb_msg <= nb_car) {
-    document.getElementById("instructions").innerHTML = chaine.substr(
-      0,
-      nb_msg
-    );
-  } else {
-    clearInterval(id);
-  }
+function changeMessage()
+{
+	nb_msg++;
+	if (nb_msg <= nb_car) {
+	document.getElementById("instructions").innerHTML = chaine.substr(0,nb_msg);
+	}
+	else {
+		clearInterval(id);
+	}
 }
 
 function afficheMessage(monTexte) {
-  nb_msg = -1;
-  chaine = monTexte;
-  nb_car = chaine.length;
-  id = setInterval("changeMessage()", 20);
+	nb_msg = -1;
+	chaine = monTexte;
+	nb_car = chaine.length;
+	id = setInterval("changeMessage()",20);
 }
-var clickX;
-var clickY;
-
-const image = document.getElementById("imageSrc").getBoundingClientRect();
-
-function printMousePos(event) {
-	clickX = event.clientX;
-	clickY = event.clientY;
-
-	const image = document.getElementById("imageSrc").getBoundingClientRect();
-
-	const posClickX = (clickX - image.x) * 100 / image.width;
-	const posClickY = (clickY - image.y) * 100 / image.height;
-
-	console.log(posClickX, posClickY);
-
-}
-
-document.addEventListener("click", printMousePos);
-
-
