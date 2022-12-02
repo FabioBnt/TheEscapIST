e5a25f1525772a87275831c2a322ae95cdc04a99
Maxiwere 45 <amdjad9857@gmail.com>
2022-12-02 03:59:50 +0100

    wesh

--- js/LivingRoom.js
+++ js/LivingRoom.js
@@ -5,9 +5,6 @@ function playAmongUsAudio(url){
     new Audio(url).play();
 }
 
-var container = document.getElementById('livingroom');
-
-
 function toggle(){
     var popup = document.getElementById('popup')
     popup.classList.toggle('active')
