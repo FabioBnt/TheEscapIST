c9927d342661858e57f09efcee5f3c62feb45a59
FabioBnt <fabiobentaila@gmail.com>
2022-12-02 03:35:15 +0100

    Création page voiture à finir

---
+++ car.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/car_style.css">
+    <title>Voiture</title>
+</head>
+<body>
+    <div class="container">
+        <div class="imageContainer">
+            <img src="./img/CarNuitInfo.png" usemap="#image-map">
+            <map name="image-map">
+                <area onclick="" coords="1487,1023,1887,1133" shape="rect">
+                <area onclick="" coords="1080,961,47" shape="circle">
+                <area title="Page précédente" href="#" coords="41,710,167,852" shape="rect">
+                <area title="Page suivante" href="#" coords="2390,728,2531,893" shape="rect">
+            </map>
+        </div>
+    </div>
+</body>
+</html>
